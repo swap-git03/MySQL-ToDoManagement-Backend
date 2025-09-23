55211b88b382c843c0da2c77692a86e972f77933
swap-git03 <swapnilfulpagare061003@gmail.com>
2025-09-23 18:43:01 +0530

    /

--- index.js
+++ index.js
@@ -1,20 +1,16 @@
 const express = require('express')
 require('dotenv').config()
 const taskRoute = require('./routes/taskRoute')
+const db = require('./config/db')  // ✅ This now gives you the connection object
 
 const app = express()
-const port = process.env.PORT || 3000
-// const dbConnect = require('./config/db')
+const port = process.env.PORT || 7002
 
-app.get('/', (req,res) => res.send('helo'))
+app.use(express.json())  // ✅ Body parser must come before routes
+
+app.get('/', (req, res) => res.send('helo'))
 app.use('/task', taskRoute)
 
-
-
-app.get('/', (req, res) => res.send('Hello World!'))
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
-// http://localhost:7002/task/getAllTasks
-
-// http://localhost:7002/task/createTask
-
+module.exports = db  // ✅ Export it if controllers need direct access
